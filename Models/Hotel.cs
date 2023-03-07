58365e02b7a106eb78081007fdcfff2040f71bcd
genwatt <adriankosoglos@gmail.com>
2023-03-07 20:00:36 +0100

    Add project1

---
+++ Models/Hotel.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper.Configuration;
+
+namespace Projekt1.Models
+{
+    public sealed class HotelMap : ClassMap<Hotel>
+    {
+        public HotelMap()
+        {
+            Map(x => x.Lp).Index(0);
+            Map(x => x.Name).Index(1);
+            Map(x => x.PhoneNumber).Index(2);
+            Map(x => x.Email).Index(3);
+            Map(x => x.Services).Index(4);
+            Map(x => x.Category).Index(5);
+            Map(x => x.Kind).Index(6);
+            Map(x => x.Adress).Index(7);
+        }
+    }
+    public class Hotel
+    {
+        public int Lp { get; set; }
+        public string Name { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Services { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string Kind { get; set; } = "";
+        public string Adress { get; set; } = "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using System.Globalization;
+using Projekt1.Models;
+using CsvHelper.Configuration;
+
+public class Program
+{
+    static void DisplayHotel(Hotel hotel)
+    {
+        Console.WriteLine($"Lp: {hotel.Lp}, Name: {hotel.Name}, Phone number: {hotel.PhoneNumber} Hotel Category: {hotel.Category}");
+    }
+    static void DisplayHotels(IEnumerable<Hotel> hotels)
+    {
+        foreach (var hotel in hotels)
+        {
+            DisplayHotel(hotel);
+        }
+    }
+
+    static void Main()
+    {
+        //  zad 1
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";"
+        };
+
+        using (var reader = new StreamReader("hotele.csv"))
+        using (var csv = new CsvReader(reader, config))
+        {
+            csv.Context.RegisterClassMap<HotelMap>();
+            var hotels = csv.GetRecords<Hotel>().ToList();
+            // 3. Obliczyć ile hoteli ma charakter sezonowy
+            var seasonHotelsCount = hotels.Where(h => h.Services == "sezonowy").Count();
+            Console.WriteLine("3. Obliczyć ile hoteli ma charakter sezonowy");
+            Console.WriteLine($"{seasonHotelsCount} hotele");
+            // 6.Wyświetlić hotele, które pochodzą z okolicy Bielska - Białej(numer telefonu zaczyna się 33)
+            var hotelsFromBielsko = hotels.Where(h => h.PhoneNumber.StartsWith("33"));
+            Console.WriteLine("6. Wyświetlić hotele, które pochodzą z okolicy Bielska-Białej (numer telefonu zaczyna się 33)");
+            DisplayHotels(hotelsFromBielsko);
+            //7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie
+            var hotelsCountInCategory = hotels
+                .GroupBy(h => h.Category)
+                .Select(h => new { Category = h.Key, hotelCount = h.Count() });
+            Console.WriteLine("7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie");
+            foreach (var item in hotelsCountInCategory)
+            {
+                Console.WriteLine($"Category name: {item.Category}, hotel count: {item.hotelCount}");
+            }
+        }
+    }
+}
