a905a98bd3c9c16d95ecc987e68fdc26f2427c41
Mzuz <mikizuziak@gmail.com>
2023-03-09 10:17:36 +0100

    MZ: Refactor code & improve console output readability & mark tast done by AR

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Projekt1;
 
 public class Program
 {
+    //Utility functions
     static void DisplayHotel(Hotel hotel)
     {
         Console.WriteLine($"Lp: {hotel.Lp,4} | Name: {hotel.Name,40} | Phone number: {hotel.PhoneNumber,16} | Hotel Category: {hotel.Category,6}");
@@ -17,35 +18,76 @@ public class Program
         }
     }
 
+    static void DisplaySpacer()
+    {
+        Console.WriteLine("\n------------------\n");
+    }
+
     static void Main()
     {
-        //  zad 1
+        //[AR]1. Wczytać dane z pliku hotele.csv z użyciem biblioteki CsvHelper
         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
             Delimiter = ";"
         };
 
-        using (var reader = new StreamReader("hotele.csv"))
+        const string csvFile = "hotele.csv";
+
+        using (var reader = new StreamReader(csvFile))
         using (var csv = new CsvReader(reader, config))
         {
-            var hotels = csv.GetRecords<Hotel>().ToList();
-            // 3. Obliczyć ile hoteli ma charakter sezonowy
-            var seasonHotelsCount = hotels.Where(h => h.Services == "sezonowy").Count();
-            Console.WriteLine("3. Obliczyć ile hoteli ma charakter sezonowy");
-            Console.WriteLine($"{seasonHotelsCount} hotele");
-            // 6.Wyświetlić hotele, które pochodzą z okolicy Bielska - Białej(numer telefonu zaczyna się 33)
-            var hotelsFromBielsko = hotels.Where(h => h.PhoneNumber.StartsWith("33"));
-            Console.WriteLine("6. Wyświetlić hotele, które pochodzą z okolicy Bielska-Białej (numer telefonu zaczyna się 33)");
+            List<Hotel> hotels = csv.GetRecords<Hotel>().ToList();
+
+            Console.WriteLine($"Info: Załadowano {hotels.Count()} pozycji z pliku {csvFile}");
+
+            DisplaySpacer();
+
+            //[AR]3. Obliczyć ile hoteli ma charakter sezonowy
+
+            const string searchedKind = "sezonowy";
+
+            var seasonHotelsCount = hotels
+                .Where(h => h.Services.ToLower() == searchedKind)
+                .Count();
+
+            Console.WriteLine("Zadnanie 3. Obliczyć ile hoteli ma charakter sezonowy\n");
+            Console.WriteLine($"Hoteli sezonowych jest: {seasonHotelsCount}");
+
+            DisplaySpacer();
+
+            //[AR]6.Wyświetlić hotele, które pochodzą z okolicy Bielska - Białej(numer telefonu zaczyna się 33)
+
+            const string bielskoBialaAreaCode = "33";
+
+            var hotelsFromBielsko = hotels
+                .Where(h => h.PhoneNumber.StartsWith(bielskoBialaAreaCode));
+
+            Console.WriteLine("Zadanie 6. Wyświetlić hotele, które pochodzą z okolicy Bielska-Białej (numer telefonu zaczyna się 33)\n");
+            Console.WriteLine("Hotele z okolicy Bielska-Białej:");
+
             DisplayHotels(hotelsFromBielsko);
-            //7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie
+
+            DisplaySpacer();
+
+            //[AR]7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie
+
             var hotelsCountInCategory = hotels
                 .GroupBy(h => h.Category)
-                .Select(h => new { Category = h.Key, hotelCount = h.Count() });
-            Console.WriteLine("7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie");
+                .Select(h => new {
+                    Category = h.Key,
+                    hotelCount = h.Count()
+                });
+
+            Console.WriteLine("Zadanie 7. Pogrupować hotele wg kategorii i zwrócić ile hoteli występuje w każdej grupie\n");
+
+            Console.WriteLine("Kategorie i ich liczebnosć:");
+
             foreach (var item in hotelsCountInCategory)
             {
-                Console.WriteLine($"Category name: {item.Category}, hotel count: {item.hotelCount}");
+                Console.WriteLine($"Category name: {item.Category,8}, hotel count: {item.hotelCount,4}");
             }
+
+            DisplaySpacer();
         }
     }
 }
